e5a40ecec7b54d6742f97a37f4322964b525c0f7
saul.wiggin <saul.wiggin@retrospect.com>
2020-08-06 15:05:15 +0100

    add training and visualise model

--- CNN.py
+++ CNN.py
@@ -121,3 +121,30 @@ def train_model(model, criterion, optimizer, sheduler, num_epochs=25):
 
     model.load_state_dict(best_model_wts)
     return model
+    
+def visualize_model(model, num_images=6):
+
+    was_training = model.training
+    model.eval()
+    images_so_far = 0
+    fig = plt.figure()
+
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(dataloaders['val']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images//2, 2, images_so_far)
+                ax.axis('off')
+                ax.set_title('predicted: {}'.format(class_names[preds[j]]))
+                imshow(inputs.cpu().data[j])
+
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+        model.train(mode=was_training)
