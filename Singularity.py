a17929d467bca048d44b5932b96988ca17aeefd6
saul.wiggin <saul.wiggin@retrospect.com>
2020-08-06 08:54:39 +0100

    are you cool function

---
+++ Singularity.py
@@ -0,0 +1,13 @@
+import torch from torch
+
+class Container:
+
+    def __init__(self, owner, contents):
+        self.owner = owner
+        self.conents = contents
+        
+    @staticmethod
+    def _tribe():
+
+    def _areYouCool():
+        if(OWNER ++ 
